ed4a8524390e59c0d95f5a8dba25474632bb33b3
Espen Moen Eilertsen <e.m.eilertsen@psykologi.uio.no>
2021-10-19 21:24:17 +0200

    mM

--- src/linalg.jl
+++ src/linalg.jl
@@ -1,7 +1,7 @@
 
 # Only fill the upper triangle
 # use T1 and T2 so that C may be of different type than B
-function muladduppertri!(C::Matrix{T}, α::Number, B::Symmetric{T, AbstractMatrix{T}}) where {T<:AbstractFloat}
+function muladduppertri!(C::Matrix{T}, α::Number, B::Symmetric{T, Matrix{T}}) where {T<:AbstractFloat}
     for i ∈ 1:size(C, 2)
         for j ∈ 1:i
            C[j,i] += α * B[j,i]
@@ -10,7 +10,7 @@ function muladduppertri!(C::Matrix{T}, α::Number, B::Symmetric{T, AbstractMatri
    C #Symmetric(C, :U)
 end
 
-function muladduppertri!(C::Matrix{T1}, α::Number, B::Symmetric{T2, AbstractMatrix{T2}}) where {T1<:AbstractFloat, T2<:AbstractFloat}
+function muladduppertri!(C::Matrix{T1}, α::Number, B::Symmetric{T2, Matrix{T2}}) where {T1<:AbstractFloat, T2<:AbstractFloat}
     for i ∈ 1:size(C, 2)
         for j ∈ 1:i
            C[j,i] += α * B[j,i]
